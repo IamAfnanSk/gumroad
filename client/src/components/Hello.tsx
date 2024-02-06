18e81c63f3097ec4dccc81be1b2a10bb66f920d8
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2024-02-06 22:32:10 +0530

    feat: correct tailwind setup completed

--- client/src/components/Hello.tsx
+++ client/src/components/Hello.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import './styles.css'
 
 const Hello = () => {
-  return <h1 className="font-bold test-hello">Hello</h1>
+  return <h1 className="test-hello">Hello</h1>
 }
 
 export { Hello }
